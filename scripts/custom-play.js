eaac69d713c7458524bc16c01a969a083e5d9ed0
Warren Li <warrenli.me@gmail.com>
2021-07-27 13:14:32 -0400

    temp remove broken logging

--- scripts/custom-play.js
+++ scripts/custom-play.js
@@ -1,13 +1,13 @@
-import { AWSAPIGatewayWrapper } from "@educational-technology-collective/etc_http_aws_api_gateway_wrapper";
-
-let timestamp: number = Date.now();
-
-let awsAPIGatewayWrapper: AWSAPIGatewayWrapper = new AWSAPIGatewayWrapper(
-    {
-        url: "https://example.com",
-        bucket: "the-name-of-the-bucket",
-        path: "the-path", // e.g., path/to/resource
-    });
+// import { AWSAPIGatewayWrapper } from "@educational-technology-collective/etc_http_aws_api_gateway_wrapper";
+//
+// let timestamp: number = Date.now();
+//
+// let awsAPIGatewayWrapper: AWSAPIGatewayWrapper = new AWSAPIGatewayWrapper(
+//     {
+//         url: "https://example.com",
+//         bucket: "the-name-of-the-bucket",
+//         path: "the-path", // e.g., path/to/resource
+//     });
 
 var playButton = document.getElementById("play_button");
 // Event listener for the play/pause button
@@ -16,12 +16,12 @@ changeButtonText = function() {
   if (video.paused == true) {
     video.play();
     playButton.innerHTML = "Pause ⏸️";
-    awsAPIGatewayWrapper.request(["Play", timestamp]);
+    //awsAPIGatewayWrapper.request(["Play", timestamp]);
   }
   else {
     video.pause();
     playButton.innerHTML = "Play ▶️";
-    awsAPIGatewayWrapper.request(["Pause", timestamp]);
+    //awsAPIGatewayWrapper.request(["Pause", timestamp]);
   }
 }
 
